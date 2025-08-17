fb06c3efb07030d4cacfa273ea4dbd14578977fc
Sgtpealz <momhmomhmomh@gmail.com>
2025-08-17 16:53:01 +0200

    Add files via upload

--- discord-wowaudit-bot/wow_data.py
+++ discord-wowaudit-bot/wow_data.py
@@ -3,39 +3,43 @@
 # Character data from provided list
 CHARACTER_MAP = {
     4343606: "Hannox",
-    4440556: "Insebtion",
-    4459420: "Iyásu",
+    4509395: "Insebtion",
+    4509392: "Iyásu",
     3938881: "Dengø",
     4342796: "Pudu",
-    4414716: "Barrageobamá",
-    4411771: "Greenmelt",
+    4509469: "Greenmelt",
     4389535: "Hugeheals",
-    4460945: "Thalvir",
+    4509394: "Thalvir",
     4225318: "Akaï",
     4339466: "Balest",
+    4511399: "Eichelherr",
+    4511400: "Jozsef",
+    4511392: "Qlaun",
     4330503: "Valeeh",
     4397102: "Lattenseppi",
     4157476: "Böllercolada",
-    4440555: "Farnis",
+    4509473: "Farnis",
     3938886: "Stormi",
-    4343631: "Bloody",
-    4388727: "Ayutra",
+    4509467: "Bloody",
     4154328: "Balî",
-    4411772: "Mîlanîa",
+    4507210: "Hyutra",
     3799853: "Naish",
     4414793: "Narune",
     4463918: "Nedbigbi",
-    3799860: "Noirrion",
+    4509466: "Noirrion",
+    4500257: "Sorfilia",
     4414724: "Thylin",
-    3875920: "Vanitas",
+    4509246: "Vanitas",
     3938887: "Zidina",
+    4505701: "Soriba",
     3969334: "Senkai",
     4285787: "Taypsi",
-    4446765: "Lavalexi",
+    4509393: "Lavalexi",
     3938880: "Huntjinn",
-    4417993: "Inancabi",
     4011638: "Justíce",
-    3799864: "Æoreth",
+    4509470: "Kraftbrühe",
+    4505171: "Zilas",
+    4509248: "Æoreth",
     4413813: "Sneakerz"
 }
 
@@ -47,34 +51,38 @@ CHARACTER_CLASSES = {
     "Iyásu": ("Shaman", "Heal"),
     "Dengø": ("Paladin", "Melee"),
     "Pudu": ("Paladin", "Heal"),
-    "Barrageobamá": ("Demon Hunter", "Melee"),
     "Greenmelt": ("Evoker", "Ranged"),
     "Hugeheals": ("Paladin", "Melee"),
     "Thalvir": ("Death Knight", "Melee"),
     "Akaï": ("Monk", "Melee"),
     "Balest": ("Shaman", "Heal"),
+    "Eichelherr": ("Druid", "Ranged"),
+    "Jozsef": ("Rogue", "Melee"),
+    "Qlaun": ("Warlock", "Ranged"),
     "Valeeh": ("Priest", "Heal"),
     "Lattenseppi": ("Warrior", "Melee"),
     "Böllercolada": ("Warlock", "Ranged"),
     "Farnis": ("Druid", "Ranged"),
-    "Stormi": ("Death Knight", "Tank"),
+    "Stormi": ("Death Knight", "Melee"),
     "Bloody": ("Warrior", "Melee"),
-    "Ayutra": ("Evoker", "Ranged"),
     "Balî": ("Shaman", "Heal"),
-    "Mîlanîa": ("Death Knight", "Melee"),
+    "Hyutra": ("Mage", "Ranged"),
     "Naish": ("Priest", "Ranged"),
     "Narune": ("Druid", "Ranged"),
     "Nedbigbi": ("Death Knight", "Melee"),
     "Noirrion": ("Hunter", "Ranged"),
+    "Sorfilia": ("Demon Hunter", "Melee"),
     "Thylin": ("Monk", "Tank"),
     "Vanitas": ("Mage", "Ranged"),
     "Zidina": ("Shaman", "Ranged"),
+    "Soriba": ("Druid", "Melee"),
     "Senkai": ("Warlock", "Ranged"),
     "Taypsi": ("Priest", "Heal"),
     "Lavalexi": ("Druid", "Heal"),
     "Huntjinn": ("Hunter", "Ranged"),
-    "Inancabi": ("Mage", "Ranged"),
     "Justíce": ("Paladin", "Melee"),
+    "Kraftbrühe": ("Paladin", "Melee"),
+    "Zilas": ("Demon Hunter", "Melee"),
     "Æoreth": ("Warrior", "Tank"),
     "Sneakerz": ("Rogue", "Melee")
 }
@@ -82,18 +90,20 @@ CHARACTER_CLASSES = {
 # Group by raid role (based on "role" in data)
 CHARACTER_ROLES = {
     "Tanks und Heiler": [
-        "Pudu", "Iyásu", "Balest", "Valeeh", "Balî", "Thylin", "Æoreth", "Stormi", "Taypsi", "Lavalexi"
+        "Pudu", "Iyásu", "Balest", "Valeeh", "Balî", "Thylin", "Æoreth",
+        "Taypsi", "Lavalexi"
     ],
     "Melees": [
-        "Insebtion", "Dengø", "Barrageobamá", "Hugeheals", "Thalvir", "Akaï", "Lattenseppi",
-        "Bloody", "Mîlanîa", "Nedbigbi", "Justíce", "Sneakerz"
+        "Insebtion", "Dengø", "Hugeheals", "Thalvir", "Akaï", "Lattenseppi",
+        "Bloody", "Nedbigbi", "Sorfilia", "Justíce", "Kraftbrühe", "Zilas",
+        "Sneakerz", "Jozsef", "Soriba", "Stormi"
     ],
     "Ranges": [
-        "Hannox", "Greenmelt", "Böllercolada", "Farnis", "Ayutra", "Naish", "Narune", "Noirrion",
-        "Vanitas", "Zidina", "Senkai", "Huntjinn", "Inancabi"
+        "Hannox", "Greenmelt", "Eichelherr", "Qlaun", "Böllercolada", "Farnis",
+        "Hyutra", "Naish", "Narune", "Noirrion", "Vanitas", "Zidina",
+        "Senkai", "Huntjinn"
     ]
 }
-
 
 # Class colors (hex)
 CLASS_HEX_COLORS = {
